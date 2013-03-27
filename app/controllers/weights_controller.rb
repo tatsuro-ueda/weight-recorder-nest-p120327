b52c01cb502e27323a28d62fbea58f0c840c9090
Tatsuro Ueda <ueda_tatsuro@yahoo.co.jp>
2013-03-27 17:34:41 +0900

    藤本さんにアドバイスいただいた

--- app/controllers/weights_controller.rb
+++ app/controllers/weights_controller.rb
@@ -2,7 +2,8 @@ class WeightsController < ApplicationController
   # GET /weights
   # GET /weights.json
   def index
-    @weights = Weight.all
+    @user = User.find(params[:user_id])
+    @weights = @user.weight.all
 
     respond_to do |format|
       format.html # index.html.erb
@@ -13,7 +14,8 @@ class WeightsController < ApplicationController
   # GET /weights/1
   # GET /weights/1.json
   def show
-    @weight = Weight.find(params[:id])
+    @user = User.find(params[:user_id])
+    @weight = @user.weight.find(params[:id])
 
     respond_to do |format|
       format.html # show.html.erb
@@ -24,42 +26,71 @@ class WeightsController < ApplicationController
   # GET /weights/new
   # GET /weights/new.json
   def new
-    @weight = Weight.new
+    @user = User.find(params[:user_id])
+    @weight = @user.weight.build
+    #respond_with(@weight)
 
     respond_to do |format|
-      format.html # new.html.erb
+      format.html # show.html.erb
       format.json { render json: @weight }
     end
   end
 
+  def create
+    @user = User.find(params[:user_id])
+    @weight = @user.weight.build(params[:weight])
+
+    respond_to do |format|
+      if @weight.save
+        format.html { redirect_to [@user, @weight], notice: 'Weight was successfully created.' }
+        format.json { render json: @weight, status: :created, location: @weight }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @weight.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  #def new
+  #  @weight = Weight.new
+  #  logger.debug "@weight=#{@weight.to_yaml}"
+  #
+  #  respond_to do |format|
+  #    format.html # new.html.erb
+  #    format.json { render json: @weight }
+  #  end
+  #end
+
   # GET /weights/1/edit
   def edit
-    @weight = Weight.find(params[:id])
+    @user = User.find(params[:user_id])
+    @weight = @user.weight.find(params[:id])
   end
 
   # TODO should be able to enter weight
   # POST /weights
   # POST /weights.json
-  def create
-    user = User.find params[:user_id]
-    user.weight.create params[:weight]
-    #@weight = Weight.new(params[:weight])
-    #
-    #respond_to do |format|
-    #  if @weight.save
-    #    format.html { redirect_to @weight, notice: 'Weight was successfully created.' }
-    #    format.json { render json: @weight, status: :created, location: @weight }
-    #  else
-    #    format.html { render action: "new" }
-    #    format.json { render json: @weight.errors, status: :unprocessable_entity }
-    #  end
-    #end
-  end
+  #def create
+  #  user = User.find params[:user_id]
+  #  user.weight.create params[:weight]
+  #  #@weight = Weight.new(params[:weight])
+  #  #
+  #  #respond_to do |format|
+  #  #  if @weight.save
+  #  #    format.html { redirect_to @weight, notice: 'Weight was successfully created.' }
+  #  #    format.json { render json: @weight, status: :created, location: @weight }
+  #  #  else
+  #  #    format.html { render action: "new" }
+  #  #    format.json { render json: @weight.errors, status: :unprocessable_entity }
+  #  #  end
+  #  #end
+  #end
 
   # PUT /weights/1
   # PUT /weights/1.json
   def update
-    @weight = Weight.find(params[:id])
+    @user = User.find(params[:user_id])
+    @weight = @user.weight.find(params[:id])
 
     respond_to do |format|
       if @weight.update_attributes(params[:weight])
@@ -75,7 +106,8 @@ class WeightsController < ApplicationController
   # DELETE /weights/1
   # DELETE /weights/1.json
   def destroy
-    @weight = Weight.find(params[:id])
+    @user = User.find(params[:user_id])
+    @weight = @user.weight.find(params[:id])
     @weight.destroy
 
     respond_to do |format|
